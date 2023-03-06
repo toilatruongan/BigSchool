e4ef8cf657e915a4804bef4a7aa6b4489670b8cc
84917 <84917@LAPTOP-1LMJI7FF>
2023-03-06 22:39:46 +0700

    Add project files.

---
+++ Migrations/202303061440201_CreateTableCategoryCourse.cs
@@ -0,0 +1,49 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableCategoryCourse : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LecturerId = c.String(nullable: false, maxLength: 128),
+                        Place = c.String(nullable: false, maxLength: 255),
+                        DateTime = c.DateTime(nullable: false),
+                        CategoryId = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.LecturerId, cascadeDelete: true)
+                .Index(t => t.LecturerId)
+                .Index(t => t.CategoryId);
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES(1,'Development')");
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES(2,'Business')");
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES(3,'Marketing')");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "LecturerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            DropIndex("dbo.Courses", new[] { "LecturerId" });
+            DropTable("dbo.Courses");
+            DropTable("dbo.Categories");
+        }
+    }
+}
